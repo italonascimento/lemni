476291e275079c97be217306c17432b539e2a872
Italo Nascimento <italo.abnascimento@gmail.com>
2018-02-03 11:51:11 -0200

    implement test of emitter.emit

--- tests/events.test.tsx
+++ tests/events.test.tsx
@@ -3,7 +3,8 @@ import 'jest'
 import * as React from 'react'
 import { Stream } from 'xstream'
 import xs from 'xstream'
-import reuse from '../src/reuse'
+import reuse from '../src/reuse';
+import sampleCombine from 'xstream/extra/sampleCombine';
 
 interface State {
   count: number
@@ -38,7 +39,34 @@ const EventsComp = reuse<{}, State>(sources => {
         <p>Counter: {state.count}</p>
         <button className='increment' onClick={emitter.signal(increment)}>Increment</button>
         <button className='incrementBy2' onClick={emitter.emitValue(2)(incrementBy)}>Increment by 2</button>
+        <IncrementButton by={3} onClick={emitter.emit(incrementBy)}/>
       </div>
+    )
+  }
+})
+
+interface IncrementProps {
+  by: number
+  onClick: (v: number) => void
+}
+
+const IncrementButton = reuse<IncrementProps>(sources => {
+  const onClickEvent = Stream.create<undefined>()
+
+  return {
+    sideEffect: onClickEvent
+      .compose(sampleCombine(
+        sources.props
+      ))
+      .map(([_1, _2]) => _2)
+      .map(({by, onClick}) =>
+        () => {
+          onClick(by)
+        }
+      ),
+
+    view: (props, state, emitter) => (
+      <button onClick={emitter.signal(onClickEvent)}></button>
     )
   }
 })
@@ -52,4 +80,7 @@ test('Events', () => {
 
   wrapper.find('.incrementBy2').simulate('click')
   expect(wrapper.find('p').text()).toBe('Counter: 3')
+
+  wrapper.find(IncrementButton).simulate('click')
+  expect(wrapper.find('p').text()).toBe('Counter: 6')
 })
