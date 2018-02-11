3d6f5279b59ef1efd1ce55b8b7e13f3e200209f1
Italo Nascimento <italo.abnascimento@gmail.com>
2018-02-11 01:58:28 -0200

    clean branch

--- packages/reuse-core/src/react-lifecycle.ts
+++
@@ -1,9 +0,0 @@
-export enum ReactLifecycle {
-  componentDidMount = 'componentDidMount',
-  componentWillUnmount = 'componentWillUnmount',
-  componentWillMount = 'componentWillMount',
-  componentWillReceiveProps = 'componentWillReceiveProps',
-  shouldComponentUpdate = 'shouldComponentUpdate',
-  componentWillUpdate = 'componentWillUpdate',
-  componentDidUpdate = 'componentDidUpdate',
-}
